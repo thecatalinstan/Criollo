5e8248cd0257b975da1cd0a6468b12cf8573e7c3
Cătălin Stan <catalin.stan@me.com>
2015-09-19 21:04:11 +0200

    .

---
+++ Criollo/Classes/CRServer.h
@@ -0,0 +1,13 @@
+//
+//  CRServer.h
+//  Criollo
+//
+//  Created by Catalin Stan on 7/24/15.
+//  Copyright (c) 2015 Cătălin Stan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CRServer : NSObject
+
+@end
