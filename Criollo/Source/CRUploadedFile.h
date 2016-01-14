244f798d9ff57c0aa5da63806bdf29c6f13e3cd3
Cătălin Stan <catalin.stan@me.com>
2016-01-14 17:17:22 +0100

    Added UploadFile

---
+++ Criollo/Source/CRUploadedFile.h
@@ -0,0 +1,18 @@
+//
+//  CRUploadedFile.h
+//  Criollo
+//
+//  Created by Cătălin Stan on 1/14/16.
+//  Copyright © 2016 Cătălin Stan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CRUploadedFile : NSObject
+
+@property (nonatomic, strong, nonnull) NSString * name;
+@property (nonatomic, strong, nonnull) NSString * path;
+@property (nonatomic, strong, nonnull) NSString * size;
+@property (nonatomic, strong, nonnull) NSString * type;
+
+@end
