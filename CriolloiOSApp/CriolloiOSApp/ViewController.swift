cd8a3fd4d92e37f60b6e0777ec8d926d8906556d
Cătălin Stan <catalin.stan@me.com>
2016-03-27 16:09:14 +0200

    Added iOS embeddable app

---
+++ CriolloiOSApp/CriolloiOSApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CriolloiOSApp
+//
+//  Created by Cătălin Stan on 27/03/16.
+//  Copyright © 2016 Cătălin Stan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
