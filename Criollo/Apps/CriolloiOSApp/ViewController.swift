ff9825cf553836d421a4ef5e10bb9ac4bef49545
Cătălin Stan <catalin.stan@me.com>
2016-03-31 01:53:26 +0200

    Initial iOS target setup

--- Criollo/Apps/CriolloiOSApp/ViewController.swift
+++ Criollo/Apps/CriolloiOSApp/ViewController.swift
@@ -8,7 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextViewDelegate {
+
+    @IBOutlet weak var logTextView: UITextView!;
+
+    @IBOutlet weak var statusImageItem: UIBarButtonItem!;
+    @IBOutlet weak var startItem: UIBarButtonItem!;
+    @IBOutlet weak var stopItem: UIBarButtonItem!;
+
+    @IBOutlet weak var toolbar: UIToolbar!;
+    @IBOutlet weak var statusDetailsButton: UILabel!;
+
+    weak var appDelegate: AppDelegate!;
+    strong var linkChecker: NSDataDetector!;
 
     override func viewDidLoad() {
         super.viewDidLoad()
