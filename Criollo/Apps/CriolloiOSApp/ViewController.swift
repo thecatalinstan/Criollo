8a73715c96d77eada1b618c14556113d88aeefdf
Cătălin Stan <catalin.stan@me.com>
2016-03-31 13:28:42 +0200

    Basic iOS app structure

--- Criollo/Apps/CriolloiOSApp/ViewController.swift
+++ Criollo/Apps/CriolloiOSApp/ViewController.swift
@@ -10,18 +10,6 @@ import UIKit
 
 class ViewController: UIViewController, UITextViewDelegate {
 
-    @IBOutlet weak var logTextView: UITextView!;
-
-    @IBOutlet weak var statusImageItem: UIBarButtonItem!;
-    @IBOutlet weak var startItem: UIBarButtonItem!;
-    @IBOutlet weak var stopItem: UIBarButtonItem!;
-
-    @IBOutlet weak var toolbar: UIToolbar!;
-    @IBOutlet weak var statusDetailsButton: UILabel!;
-
-    weak var appDelegate: AppDelegate!;
-    strong var linkChecker: NSDataDetector!;
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
