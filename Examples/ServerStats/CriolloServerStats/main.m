aaeb2f859de1da20934b7b2cfa85ff848f86a745
Cătălin Stan <cas@phaseone.com>
2017-10-01 13:25:07 +0200

    Add ServerStats example app

---
+++ Examples/ServerStats/CriolloServerStats/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  CriolloServerStats
+//
+//  Created by Cătălin Stan on 28/09/2017.
+//  Copyright © 2017 Cătălin Stan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
