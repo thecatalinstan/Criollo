ec128628ef69eeae0c90ab2dfb80fe94a02b8b12
Cătălin Stan <catalin.stan@me.com>
2017-10-23 21:44:57 +0200

    Add tvOS app

---
+++ Criollo/Apps/CriolloTVApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CriolloTVApp
+//
+//  Created by Cătălin Stan on 23/10/2017.
+//  Copyright © 2017 Cătălin Stan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
