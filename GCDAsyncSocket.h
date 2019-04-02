534dac6e1e519014e0c02d8b145d5e352bfad233
Adam Fedor <afedor@t1v.com>
2019-04-02 15:55:56 -0600

    Use the GCDAsyncSocket framework rather than importing the files directly

---
+++ GCDAsyncSocket.h
@@ -0,0 +1,14 @@
+//
+//  GCDAsyncSocket.h
+//  Criollo
+//
+//  Created by Adam Fedor on 4/2/19.
+//  Copyright © 2019 Cătălin Stan. All rights reserved.
+//
+
+#ifndef GCDAsyncSocket_h
+#define GCDAsyncSocket_h
+
+#import <CocoaAsyncSocket/GCDAsyncSocket.h>
+
+#endif /* GCDAsyncSocket_h */
