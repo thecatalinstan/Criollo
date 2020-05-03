f3a6a12ce89ee5fbee37e38e1ade290c9d2c629b
Cătălin Stan <catalin.stan@me.com>
2020-05-03 17:52:57 +0200

    Travis scripts: remove `-quiet` flag for tests

--- .scripts/apps.sh
+++ .scripts/apps.sh
@@ -4,5 +4,5 @@ set -x
 
 # Apps
 xcodebuild clean build -quiet -project Criollo.xcodeproj -scheme "Criollo macOS App" CODE_SIGNING_REQUIRED=NO
-xcodebuild clean build -quiet -project Criollo.xcodeproj -destination 'platform=iOS Simulator,name=iPhone 11' -scheme "Criollo iOS App" CODE_SIGNING_REQUIRED=NO
+xcodebuild clean build -quiet -project Criollo.xcodeproj -destination 'platform=iOS Simulator,name=iPhone 8' -scheme "Criollo iOS App" CODE_SIGNING_REQUIRED=NO
 xcodebuild clean build -quiet -project Criollo.xcodeproj -destination 'platform=tvOS Simulator,name=Apple TV' -scheme "Criollo tvOS App" CODE_SIGNING_REQUIRED=NO
--- .scripts/examples.sh
+++ .scripts/examples.sh
@@ -17,7 +17,7 @@ cd HelloWorld-MultiTarget
 pod install --silent
 xcodebuild clean build -quiet -workspace HelloWorld.xcworkspace -scheme "HelloWorld" CODE_SIGNING_REQUIRED=NO || exit $PIPESTATUS
 xcodebuild clean build -quiet -workspace HelloWorld.xcworkspace -scheme "HelloWorld-Cocoa" CODE_SIGNING_REQUIRED=NO || exit $PIPESTATUS
-xcodebuild clean build -quiet -destination 'platform=iOS Simulator,name=iPhone 11' -workspace HelloWorld.xcworkspace -scheme "HelloWorld-iOS" CODE_SIGNING_REQUIRED=NO || exit $PIPESTATUS
+xcodebuild clean build -quiet -destination 'platform=iOS Simulator,name=iPhone 8' -workspace HelloWorld.xcworkspace -scheme "HelloWorld-iOS" CODE_SIGNING_REQUIRED=NO || exit $PIPESTATUS
 xcodebuild clean build -quiet -destination 'platform=tvOS Simulator,name=Apple TV' -workspace HelloWorld.xcworkspace -scheme "HelloWorld-tvOS" CODE_SIGNING_REQUIRED=NO || exit $PIPESTATUS
 cd ../
 
@@ -38,7 +38,7 @@ cd ../
 
 cd SecureHTTPServer
 pod install --silent
-xcodebuild clean build -quiet -destination 'platform=iOS Simulator,name=iPhone 11' -workspace SecureHTTPServer.xcworkspace -scheme "SecureHTTPServer" CODE_SIGNING_REQUIRED=NO || exit $PIPESTATUS
+xcodebuild clean build -quiet -destination 'platform=iOS Simulator,name=iPhone 8' -workspace SecureHTTPServer.xcworkspace -scheme "SecureHTTPServer" CODE_SIGNING_REQUIRED=NO || exit $PIPESTATUS
 cd ../
 
 cd ServerStats
