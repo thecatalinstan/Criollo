0ab8712663f36b33511241ef9ba25e3476d84bf3
Cătălin Stan <catalin.stan@me.com>
2020-05-17 13:48:41 +0200

    Validate basic authentication for all routes

--- Examples/HTTPAuthentication/HTTPAuthentication/AppDelegate.m
+++ Examples/HTTPAuthentication/HTTPAuthentication/AppDelegate.m
@@ -23,9 +23,22 @@ static uint16_t const port = 10781;
     self.server = [[CRHTTPServer alloc] init];
     self.server.delegate = self;
     
+    __weak typeof(self) wself = self;
+    
     // Authentication filter
     [self.server add:^(CRRequest * _Nonnull request, CRResponse * _Nonnull response, CRRouteCompletionBlock  _Nonnull completion) {
-        completion();
+        
+        // The user is authenticated, proceed
+        if ([wself validateAuthotization:request.env[@"HTTP_AUTHORIZATION"]]) {
+            completion();
+            return;
+        }
+        
+        // Send an unauthorized (401), followed by a WWW-Authenticate header
+        [response setStatusCode:401 description:nil];
+        [response setValue:[NSString stringWithFormat:@"Basic realm=\"%@\"", @"asdfasdf"] forHTTPHeaderField:@"WWW-Authenticate"];
+        [response setValue:@"0" forHTTPHeaderField:@"Content-Length"];
+        [response finish];
     }];
     
     // Hello world /
@@ -80,6 +93,28 @@ static uint16_t const port = 10781;
     [sortedPaths enumerateObjectsUsingBlock:^(NSURL * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
         [CRApp logFormat:@" %@", obj.absoluteString];
     }];
+}
+
+#pragma mark - Authentication
+
+- (BOOL)validateAuthotization:(NSString *)authorization {
+    authorization = [authorization componentsSeparatedByString:@" "].lastObject;
+    if (!authorization.length) {
+        return NO;
+    }
+    
+    // This should be done more rigurously to support multiple languages
+    NSData *data;
+    if (!(data = [[NSData alloc] initWithBase64EncodedString:authorization options:NSDataBase64DecodingIgnoreUnknownCharacters])) {
+        return NO;
+    }
+    
+    if (!(authorization = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding])) {
+        return NO;
+    }
+    
+    // For the purspose of this demo, we'll allow any credentials
+    return YES;
 }
 
 @end
