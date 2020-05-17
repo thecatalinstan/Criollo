b23d8f0972be939e0d5ddb600cdc27c9bf810329
Cătălin Stan <catalin.stan@me.com>
2020-05-17 11:20:53 +0200

    Add HTTPAuthentiation example boilerplate

---
+++ Examples/HTTPAuthentication/HTTPAuthentication/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  HTTPAuthentication
+//
+//  Created by Cătălin Stan on 17/05/2020.
+//  Copyright © 2020 Criollo. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+}
+
+
+@end
