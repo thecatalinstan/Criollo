11e892095813c054c52959ba2aa38c5547095707
Cătălin Stan <catalin.stan@me.com>
2020-05-23 22:24:42 +0200

    Add `analyze` to built-in apps CI script

--- .scripts/apps.sh
+++ .scripts/apps.sh
@@ -3,6 +3,6 @@
 set -x
 
 # Apps
-xcodebuild clean build -quiet -project Criollo.xcodeproj -scheme "Criollo macOS App" CODE_SIGNING_REQUIRED=NO
-xcodebuild clean build -quiet -project Criollo.xcodeproj -destination 'platform=iOS Simulator,name=iPhone 8' -scheme "Criollo iOS App" CODE_SIGNING_REQUIRED=NO
-xcodebuild clean build -quiet -project Criollo.xcodeproj -destination 'platform=tvOS Simulator,name=Apple TV' -scheme "Criollo tvOS App" CODE_SIGNING_REQUIRED=NO
+xcodebuild clean build analyze -quiet -project Criollo.xcodeproj -scheme "Criollo macOS App" CODE_SIGNING_REQUIRED=NO
+xcodebuild clean build analyze -quiet -project Criollo.xcodeproj -destination 'platform=iOS Simulator,name=iPhone 8' -scheme "Criollo iOS App" CODE_SIGNING_REQUIRED=NO
+xcodebuild clean build analyze -quiet -project Criollo.xcodeproj -destination 'platform=tvOS Simulator,name=Apple TV' -scheme "Criollo tvOS App" CODE_SIGNING_REQUIRED=NO
