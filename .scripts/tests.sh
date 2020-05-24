a3a65b573a24a96b0d6f2c943845bb1148806f2b
Cătălin Stan <catalin.stan@me.com>
2020-05-24 10:42:53 +0200

    Modify test script to use xctestrun

--- .scripts/tests.sh
+++ .scripts/tests.sh
@@ -3,6 +3,14 @@
 set -x
 
 # Frameworks Test
-xcodebuild clean test -quiet -project Criollo.xcodeproj -scheme "Criollo macOS" CODE_SIGNING_REQUIRED=NO
-xcodebuild clean test -quiet -destination 'platform=iOS Simulator,name=iPhone 8' -project Criollo.xcodeproj -scheme "Criollo iOS" CODE_SIGNING_REQUIRED=NO
-xcodebuild clean test -quiet -destination 'platform=tvOS Simulator,name=Apple TV' -project Criollo.xcodeproj -scheme "Criollo tvOS" CODE_SIGNING_REQUIRED=NO
+
+xcodebuild build-for-testing -derivedDataPath "./build/macOS" -quiet -destination "platform=macOS" -project Criollo.xcodeproj -scheme "Criollo macOS" CODE_SIGNING_REQUIRED=NO
+xcodebuild test-without-building -xctestrun "$(set +x; ls -1 ./build/macOS/Build/Products/*.xctestrun|tail -n 1)" -destination "platform=macOS"
+
+xcodebuild build-for-testing -derivedDataPath "./build/iOS" -quiet -destination "platform=iOS Simulator,name=iPhone 8" -project Criollo.xcodeproj -scheme "Criollo iOS" CODE_SIGNING_REQUIRED=NO
+xcodebuild test-without-building -xctestrun "$(set +x; ls -1 ./build/iOS/Build/Products/*.xctestrun|tail -n 1)" -destination "platform=iOS Simulator,name=iPhone 8"
+
+xcodebuild build-for-testing -derivedDataPath "./build/tvOS" -quiet -destination "platform=tvOS Simulator,name=Apple TV" -project Criollo.xcodeproj -scheme "Criollo tvOS" CODE_SIGNING_REQUIRED=NO
+xcodebuild test-without-building -xctestrun "$(set +x; ls -1 ./build/tvOS/Build/Products/*.xctestrun|tail -n 1)" -destination "platform=tvOS Simulator,name=Apple TV"
+
+rm -rf "./build"
