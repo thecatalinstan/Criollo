c39b876570a41c0e06534f3f2e5726043c83b07d
Cătălin Stan <catalin.stan@me.com>
2021-06-03 23:43:07 +0200

    #69: Update iOS and tvOS apps to not use certificate and key pair to enable HTTPS

--- Apps/Criollo tvOS App/AppDelegate.swift
+++ Apps/Criollo tvOS App/AppDelegate.swift
@@ -25,20 +25,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CRServerDelegate {
         // Create the server and add some handlers to do some work
         self.server = CRHTTPServer(delegate:self)
         
-        //        // Setup HTTPS
-        //        self.server.isSecure = true
-        //
-        //        // Credentials: PKCS#12 Identity and password
-        //        self.server.identityPath = Bundle.main.path(forResource: "criollo_local", ofType: "p12")
-        //        self.server.password = "123456"
-        //
-        //        // Credentials: PEM-encoded certificate and public key
-        //        self.server.certificatePath = Bundle.main.path(forResource: "cert", ofType: "pem")
-        //        self.server.privateKeyPath = Bundle.main.path(forResource: "key", ofType: "pem")
-        //
-        //        // Credentials: DER-encoded certificate and public key
-        //        self.server.certificatePath = Bundle.main.path(forResource: "cert", ofType: "der")
-        //        self.server.privateKeyPath = Bundle.main.path(forResource: "key", ofType: "der")
+        // Setup HTTPS
+        self.server.isSecure = true
+        
+        // Credentials: PKCS#12 Identity and password
+        self.server.identityPath = Bundle.main.path(forResource: "criollo_local", ofType: "p12")
+        self.server.password = "123456"
         
         let bundle:Bundle! = Bundle.main
         
